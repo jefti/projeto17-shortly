533fb4a5a3f627e5e9879cde09dc7683c8eb1936
jefti <jeftimeira@gmail.com>
2023-08-01 20:13:42 -0300

    feature: app.js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import { express } from "express";
+import cors from "cors";
+
+const app = express;
+app.use(cors());
+app.use(express.json());
+
+const Port = 5000;
+app.listen(Port, ()=>{console.log(`Servidor rodando na porta ${Port}`)});
